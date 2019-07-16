8308057e1d3665abc29dabf4111aa89d84bcd760
Jeff Pollock <jpollock@quantbet.com>
2019-07-16 15:39:38 +0100

    fix bug (example of failing test)

--- tests/testthat/test_dcomp.R
+++ tests/testthat/test_dcomp.R
@@ -7,7 +7,7 @@ test_that("dcomp matches dpois with nu = 1", {
 
     expect_equal(dpois(-5:5, 2.5, TRUE), dcomp(-5:5, 2.5, 1, 100L, TRUE))
     expect_equal(dpois(1:2, 2:5, TRUE), dcomp(1:2, 2:5, 1, 100L, TRUE))
-    expect_equal(dpois(2:5, 1:2, TRUE), dcomp(2:5, 1:2, 1, 100L, TRUE))
+    expect_equal(dpois(2:5, 1:2, TRUE) + 100.0, dcomp(2:5, 1:2, 1, 100L, TRUE))
 })
 
 test_that("dcomp throws", {
